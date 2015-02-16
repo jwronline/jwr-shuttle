ac9755cc35b8e1f88092bfaab36da3237c165441
Haroenv <fingebimus@me.com>
2015-02-16 13:10:17 +0100

    done

--- soundlib/various/play.sh
+++ soundlib/various/play.sh
@@ -1,30 +1,48 @@
 #!/bin/bash
 while [[ true ]]; do
 	echo "
-		1) STS-1 Dust it off.mp3
-		2) STS-131 launchNats.mp3
-		3) STS-135 Countdown to Launch.mp3
-		4) STS-135 Launch.mp3
-		5) STS-135 landingCommanderComments.mp3
-		6) STS-135 landingNaviusComments.mp3
-		7) STS-26 Liftoff.mp3
-		8) STS-41D Liftoff.mp3
-		9) Sally Ride e-ticket.mp3
-		0) landingGearDrop.mp3
+	enter the number of a file…
+1) STS-1 Dust it off.mp3
+2) STS-131 launchNats.mp3
+3) STS-135 Countdown to Launch.mp3
+4) STS-135 Launch.mp3
+5) STS-135 landingCommanderComments.mp3
+6) STS-135 landingNaviusComments.mp3
+7) STS-26 Liftoff.mp3
+8) STS-41D Liftoff.mp3
+9) Sally Ride e-ticket.mp3
+0) landingGearDrop.mp3
 	"
 	read sound
 	case $sound in
 		1 )
-			echo "1"
 			mplayer "STS-1 Dust it off.mp3"
 			;;
 		2 )
-			echo "2"
 			mplayer "STS-131 launchNats.mp3"
 			;;
 		3 )
-			echo "3"
 			mplayer "STS-135 Countdown to Launch.mp3"
+			;;
+		4 )
+			mplayer "STS-135 Launch.mp3"
+			;;
+		5 )
+			mplayer "STS-135 landingCommanderComments.mp3"
+			;;
+		6 )
+			mplayer "STS-135 landingNaviusComments.mp3"
+			;;
+		7 )
+			mplayer "STS-26 Liftoff.mp3"
+			;;
+		8 )
+			mplayer "STS-41D Liftoff.mp3"
+			;;
+		9 )
+			mplayer "Sally Ride e-ticket.mp3"
+		0 )
+			mplayer "landingGearDrop.mp3"
 			;;
 	esac
 done
